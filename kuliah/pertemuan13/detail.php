aeb53d965e83ff1091b35b61576c73f94c9b92e1
Robi Firmansyah <robifirmansyah096@gmail.com>
2025-03-26 03:12:44 +0800

    Menambahkan fitur login dan session sehingga user tidak dapat mengakses halaman apabila belum login...

---
+++ kuliah/pertemuan13/detail.php
@@ -0,0 +1,72 @@
+<?php
+require 'function.php';
+$id = $_GET["id"];
+
+$mahasiswa = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+
+// var_dump($mahasiswa);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Mahasiswa</title>
+    <style>
+        .card {
+            margin: auto;
+            width: 500px !important;
+            margin-bottom: 50px !important;
+
+        }
+
+        .card a {
+            color: white !important;
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+    <h3 class="mb-10 mt-10">
+        Detail mahasiswa
+    </h3>
+    <!-- <ul>
+        <li><img src="img/<?= $mahasiswa['gambar'] ?>" alt="" srcset="" width="500"></li>
+        <li></li>
+        <li>></li>
+        <li>email : <?= $mahasiswa['email'] ?></li>
+        <li>jurusan : <?= $mahasiswa['jurusan'] ?></li>
+        <li><a href="">ubah</a> | <a href="">hapus</a></li>
+    </ul> -->
+
+    <div class="card mt-10 mb-100" style="width: 18rem;">
+        <img src="img/<?= $mahasiswa['gambar'] ?>" alt="" srcset="" width="500" class="card-img-top" alt="...">
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <p>Muda Kaya Raya, Tua Foya-foya, Mati masuk Sorga.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Cirebon</cite>
+                </footer>
+            </blockquote>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Nrp : <?= $mahasiswa['nrp'] ?></li>
+            <li class="list-group-item">Nama : <?= $mahasiswa['nama'] ?></li>
+            <li class="list-group-item">Nama : <?= $mahasiswa['email'] ?></li>
+            <li class="list-group-item">Nama : <?= $mahasiswa['jurusan'] ?></li>
+        </ul>
+        <div class="card-body">
+            <a href="hapus.php?id=<?= $mahasiswa['id'] ?>" class="card-link btn btn-danger"
+                onclick="return confirm('yakin hapus data?')">Hapus</a>
+            <a href="ubah.php?id=<?= $mahasiswa['id'] ?>" class="card-link btn btn-success">Ubah</a>
+        </div>
+        <button type="button" class="btn btn-primary"><a href="index.php">Kembali Ke Halaman Utama</a></button>
+    </div>
+</body>
+
+</html>
---
+++ kuliah/pertemuan13/function.php
@@ -0,0 +1,205 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'pw_043040023');
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+
+}
+
+function tambah($data)
+{
+    global $conn;
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $sql = "INSERT INTO mahasiswa (nama, nrp, email, jurusan, gambar)
+                                VALUES
+                                ('$nama','$nrp','$email','$jurusan','$gambar')";
+    $query = mysqli_query($conn, $sql);
+
+    return mysqli_affected_rows($conn);
+
+
+}
+
+function hapus($id)
+{
+    global $conn;
+    $sql = "DELETE FROM mahasiswa WHERE id = $id";
+    $query = mysqli_query($conn, $sql);
+    return mysqli_affected_rows($conn);
+
+}
+
+function ubah($data)
+{
+    global $conn;
+    $id = $data['id'];
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $sql = "UPDATE  mahasiswa SET
+    nama = '$nama',
+    nrp = '$nrp',
+    email = '$email',
+    jurusan = '$jurusan',
+    gambar = '$gambar'
+    WHERE id = $id;
+
+    
+    ";
+    $query = mysqli_query($conn, $sql);
+
+
+    return mysqli_affected_rows($conn);
+
+}
+
+function cari($keyword)
+{
+    global $conn;
+    $query = "SELECT * FROM mahasiswa WHERE 
+    nama LIKE '%$keyword%' OR
+    nrp LIKE  '%$keyword%' 
+    ";
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function login($data)
+{
+    global $conn;
+    $username = htmlspecialchars($data['username']);
+    $password = htmlspecialchars($data['password']);
+
+
+
+    if (query("SELECT * FROM user WHERE username = '$username' && password = '$password'")) {
+        $_SESSION['login'] = true;
+        header('location:index.php');
+        exit;
+        # code...
+    } else {
+        return [
+            'error' => true,
+            'pesan' => 'email atau password yang anda masukan salah'
+        ];
+    }
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <style>
+        a.nav-link {
+            color: white !important;
+        }
+    </style>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #323539;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="color: white;">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
